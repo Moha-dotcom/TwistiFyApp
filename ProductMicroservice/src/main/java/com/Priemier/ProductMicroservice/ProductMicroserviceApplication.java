75c139a564759b3261452cff2d27afedc2e37760
Mohamed Abdullahi <Mohamedsahal612@gmail.com>
2022-12-28 11:14:55 -0600

    adding Microservice Files

---
+++ ProductMicroservice/src/main/java/com/Priemier/ProductMicroservice/ProductMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.Priemier.ProductMicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductMicroserviceApplication.class, args);
+	}
+
+}
---
+++ UserMicroservice/src/test/java/com/Priemier/UserMicroservice/UserMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Priemier.UserMicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
